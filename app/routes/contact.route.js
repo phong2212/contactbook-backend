b96d793c5955a31a4d4ef17b7032b9bcbb716a43
phong2212 <phongb2014685@student.ctu.edu.vn>
2024-02-21 15:15:24 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/routes/contact.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const contacts = require("../controllers/contact.controller");
+
+const router = express.Router();
+
+router.route("/")
+    .get(contacts.findAll)
+    .post(contacts.create)
+    .delete(contacts.deleteAll);
+
+router.route("/favorite")
+    .get(contacts.findAllFavorites);
+
+router.route("/:id")
+    .get(contacts.findOne)
+    .get(contacts.update)
+    .delete(contacts.delete);
+
+module.exports = router;
